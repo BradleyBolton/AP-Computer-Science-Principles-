192075389e0f4278277f0ee4583dbb8fd5b85c34
BradleyBolton <42778446+BradleyBolton@users.noreply.github.com>
2019-03-26 10:05:21 -0500

    Add files via upload

---
+++ Connect4.c
@@ -0,0 +1,144 @@
+/* Bradley Bolton
+ * Connect4.c
+ * This program allows the user to play connect 4 against another user or the computer
+ * Last worked on 2/20/2019
+ */
+
+#include <stdio.h>
+#include <cs50.h>
+
+int generateNewBoard();
+int drawBoard();
+int placePiece(int rowChoice, bool player);
+int AiMove();
+void checkBoardState();
+
+
+int board[8][8];
+
+//boardState represents the number of strings of 2, 3, and 4 pieces for each player
+//indexs 0 and 3 are two length lines, 1 and 4 are three length lines, 2 and 5 are four length lines
+int boardState[6];
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Proper Usage: ./Connect4 [# of human players]\n");
+        return 1;
+    }
+    int playerCount = atoi(argv[1]);
+    if (playerCount != 1 && playerCount != 2)
+    {
+        printf("Error: Only 1 or 2 players are supported in Connect4\n");
+        return 1;
+    }
+    generateNewBoard();
+    drawBoard();
+    bool player = true;
+    int rowChoice;
+    do
+    {
+        if (player)
+        {
+            printf("Player X, select the row to drop your piece.\n");
+            rowChoice = get_int() - 1;
+        }
+        else if (playerCount == 2)
+        {
+            printf("Player O, select the row to drop your piece.\n");
+            rowChoice = get_int() - 1;
+        }
+        else
+        {
+            rowChoice = AiMove();
+        }
+        if (board[0][rowChoice] == 0)
+        {
+            placePiece(rowChoice, player);
+            player = !player;
+            drawBoard();
+        }
+        else
+        {
+            printf("Invalid move.\n");
+        }
+    } while(true);
+
+}
+
+int generateNewBoard()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int n = 0; n < 8; n++)
+        {
+            board[i][n] = 0;
+        }
+    }
+    return 0;
+}
+
+int drawBoard()
+{
+    printf(" 1   2   3   4   5   6   7   8\n");
+    for (int i = 0; i < 8; i++)
+    {
+        for (int n = 0; n < 8; n++)
+        {
+            switch (board[i][n])
+            {
+            case 0:
+                printf("[ ] ");
+                break;
+            case 1:
+                printf("[X] ");
+                break;
+            case 2:
+                printf("[O] ");
+                break;
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int placePiece(int rowChoice, bool player)
+{
+    int i = 0;
+    while (board[i + 1][rowChoice] == 0 && i < 7)
+    {
+        i++;
+    }
+    if (player)
+    {
+        board[i][rowChoice] = 1;
+    }
+    else
+    {
+        board[i][rowChoice] = 2;
+    }
+    return 0;
+}
+
+void checkBoardState()
+{
+    int centralPiece;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int n = 0; n < 8; n++)
+        {
+            centralPiece = board[i][n];
+            if(centralPiece == 1 || centralPiece == 2)
+            {
+
+            }
+        }
+    }
+}
+
+int AiMove()
+{
+    return 1;
+}
